39f2cc031eb4a26b0ec22d298f004c03b5a3e9d1
Akira Ishii <akira-ishii@united.jp>
2016-04-11 09:39:55 +0000

    validates, link_from_settings, before_action

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,8 @@ class User < ActiveRecord::Base
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
+  validates :age, numericality: { greater_than_or_equal_to: 0 }
+  validates :area, length: { maximum: 20 }
+  validates :body, length: { maximum: 100 } 
   has_secure_password
 end
