cbeeb6c3e17f39dc09a6b1648838b01d7682e5f0
Akira Ishii <akira-ishii@united.jp>
2016-04-11 15:58:55 +0000

    signup error

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,8 @@ class User < ActiveRecord::Base
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
-  validates :age, numericality: { greater_than_or_equal_to: 0 }
-  validates :area, length: { maximum: 20 }
-  validates :body, length: { maximum: 100 } 
+  validates :age, numericality: { greater_than_or_equal_to: 0 }, on: :update
+  validates :area, length: { maximum: 20 }, on: :update
+  validates :body, length: { maximum: 100 } , on: :update
   has_secure_password
 end
